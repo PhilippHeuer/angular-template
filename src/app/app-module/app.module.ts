625eceb2ca0fe3fb953a1cb6e3e6526fa2549e4e
Philipp Heuer <git@philippheuer.me>
2018-06-30 20:47:21 +0200

    feature(ux): add angular material

--- src/app/app-module/app.module.ts
+++ src/app/app-module/app.module.ts
@@ -1,8 +1,8 @@
 /**
  * Dependencies
  */
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /**
