7ecb8fd3307bf4cdb639f72eb4834e38d327c9f4
Philipp Heuer <git@philippheuer.me>
2018-07-10 21:11:51 +0200

    feature(demo): mat-tree

--- src/app/app-module/components/home/home.component.ts
+++ src/app/app-module/components/home/home.component.ts
@@ -2,6 +2,13 @@
  * Dependencies
  */
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { NestedTreeControl } from '@angular/cdk/tree';
+
+interface Classification {
+  name: string;
+  children: Classification[];
+}
 
 /**
  * Component
@@ -11,12 +18,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+
+  // data
+  public classifications: Classification[] = [
+    {
+      name: 'test',
+      children: [
+        {
+          name: 'a',
+          children: [],
+        },
+        {
+          name: 'b',
+          children: [],
+        }
+      ]
+    },
+    {
+      name: 'test2',
+      children: [
+        {
+          name: 'a',
+          children: [],
+        },
+        {
+          name: 'b',
+          children: [],
+        }
+      ]
+    }
+  ];
+
+  public treeControl = new NestedTreeControl<Classification>(node => of(node.children));
+
   /**
    * Constructor
    */
   constructor() {}
 
   public ngOnInit() {
+
   }
 
 }
