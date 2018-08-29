4c48f94efa6b280261f98e445d031254a6723a96
Philipp Heuer <git@philippheuer.me>
2018-08-29 13:58:32 +0200

    feature(ux): add font-awesome

--- src/app/app-shared/app-shared.module.ts
+++ src/app/app-shared/app-shared.module.ts
@@ -5,6 +5,17 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 /**
+ * Font Awesome
+ */
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+
+/**
  * Services
  */
 import { LoggingService } from '@app/app-core/services/logging';
@@ -18,10 +29,14 @@ import { LoggingService } from '@app/app-core/services/logging';
   imports: [
     // Angular
     CommonModule,
+    // Font-AWesome
+    FontAwesomeModule,
   ],
   exports: [
     // Angular
     CommonModule,
+    // Font-AWesome
+    FontAwesomeModule,
   ]
 })
 export class SharedModule {
